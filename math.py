92e1184f3f95718e3f34edc2c087d5bb3b2e4a34
mdfaizan <mdfaizansheikh124@gmail.com>
2022-02-22 19:17:54 +0530

    removed add fn

--- math.py
+++ math.py
@@ -1,17 +1,21 @@
 def add(x,y):
- return x+y
+ pass
  
+#Subtract implemetation
 def subtract(x,y):
- return x-y             
- 
-def multiply(x,y):              
+ return x-y             #on master branch
+
+#Multiply implemetation
+def multiply(x,y):              #on bug456 branch
  return x*y
- 
-def divide(x,y):           
+
+#Divide implemetation
+def divide(x,y):             #on master branch
  if y==0 :
        return DIVIDE_BY_ZERO_ERROR
  else :
        return x/y 
-       
+
+#square implementation
 def square(x):
    return x*x
