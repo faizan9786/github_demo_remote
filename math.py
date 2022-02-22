e2dfd960447377f50564622a7de964a7ba5b1f9a
mdfaizan <mdfaizansheikh124@gmail.com>
2022-02-22 19:09:08 +0530

    Removed headers

--- math.py
+++ math.py
@@ -1,22 +1,17 @@
-#Add implemetation
 def add(x,y):
  return x+y
  
-#Subtract implemetation
 def subtract(x,y):
- return x-y             #on master branch
-
-#Multiply implemetation
-def multiply(x,y):              #on bug456 branch
+ return x-y             
+ 
+def multiply(x,y):              
  return x*y
-
-#Divide implemetation
-def divide(x,y):             #on master branch
+ 
+def divide(x,y):           
  if y==0 :
        return DIVIDE_BY_ZERO_ERROR
  else :
        return x/y 
-
-#square implementation
+       
 def square(x):
    return x*x
