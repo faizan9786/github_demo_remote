441be34b629dda8b799b4a77716ca25b6233f23c
faizan9786 <100204398+faizan9786@users.noreply.github.com>
2022-02-22 17:51:46 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implemetation
+def add(x,y):
+ return x+y
+ 
+#Subtract implemetation
+def subtract(x,y):
+ return x-y             #on master branch
+
+#Multiply implemetation
+def multiply(x,y):              #on bug456 branch
+ return x*y
+
+#Divide implemetation
+def divide(x,y):             #on master branch
+ if y==0 :
+       return DIVIDE_BY_ZERO_ERROR
+ else :
+       return x/y 
+
+#square implementation
+def square(x):
+   return x*x
